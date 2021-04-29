0e04e894bda2087ff3a56ad60c1a8cbc1ef7b869
Joe <joetakac18@gmail.com>
2021-04-29 13:37:18 +0100

    Added JS/Button to scroll to top of entries page

--- JoeTakac_WAD/wwwroot/js/site.js
+++ JoeTakac_WAD/wwwroot/js/site.js
@@ -37,3 +37,20 @@ anime.timeline({ loop: true })
         easing: "easeOutExpo",
         delay: 1000
     });
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
